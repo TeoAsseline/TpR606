9e5fc7e505968c4a3626e1d105d33590cbdd24aa
Téo Asseline <asselineteo@gmail.com>
2024-02-09 09:36:21 +0100

    changement des noms de test

--- src/Scenario.java
+++ src/Scenario.java
@@ -21,7 +21,7 @@ public class Scenario {
 	}
 	@Test
 	// test location film normal d'une duree inferieure a 3 jours
-	public void test1() {
+	public void testlocationfilmnormalmoinsde3jours() {
 		Scenario sc = new Scenario();
 		String attendu  = "Situation du client: un client\n"
 						+ "\tTaxi Driver\t2.0\n"
@@ -32,7 +32,7 @@ public class Scenario {
 	}
 	@Test
 	// test location film normal d'une duree d'au moins 3 jours
-	public void test2() {
+	public void testlocationfilmnormalplusde3jours() {
 		Scenario sc = new Scenario();
 		String attendu  = "Situation du client: un client\n"
 			+ "\tTaxi Driver\t3.5\n"
@@ -43,7 +43,7 @@ public class Scenario {
 	}
 	@Test
 	// test location film nouveauté d'une duree inferieure à 2 jours
-	public void test3() {
+	public void testlocationfilmnouveautemoinsde2jours() {
 		Scenario sc = new Scenario();
 		String attendu  = "Situation du client: un client\n"
 		+ "\t11 heures 14\t3.0\n"
@@ -54,7 +54,7 @@ public class Scenario {
 	}
 	@Test
 	// test location film nouveaute d'une duree d'au moins 2 jours
-	public void test4() {
+	public void testlocationfilmnouveauteplusde2jours() {
 		Scenario sc = new Scenario();
 		String attendu  = "Situation du client: un client\n"
 		+ "\t11 heures 14\t12.0\n"
@@ -65,7 +65,7 @@ public class Scenario {
 	}
 	@Test
 	// test location film enfant d'une duree inf�rieure a 4 jours
-	public void test5() {
+	public void testlocationfilmenfantmoinsde4jours() {
 		Scenario sc = new Scenario();
 		String attendu  = "Situation du client: un client\n"
 		+ "\tCendrillon\t1.5\n"
@@ -76,7 +76,7 @@ public class Scenario {
 	}
 	@Test
 	// test location film enfant d'une duree d'au moins 4 jours
-	public void test6() {
+	public void testlocationfilmenfantplusde4jours() {
 		Scenario sc = new Scenario();
 		String attendu  = "Situation du client: un client\n"
 		+ "\tCendrillon\t3.0\n"
@@ -87,7 +87,7 @@ public class Scenario {
 	}
 	@Test
 	// test cumul de locations
-	public void test7() {
+	public void testcumullocations() {
 		Scenario sc = new Scenario();
 		Client unClient = new Client("client cumul");
 		sc.testSituationCumul(unClient,"Taxi Driver",Film.NORMAL,2);
